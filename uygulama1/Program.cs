4fd01d71caa4ec947c8bc9a39d491ba8a032d655
EdaOzge <eozge.ugurlu@tedu.edu.tr>
2024-12-26 22:43:26 +0300

    readme dosyası silindi

--- uygulama1/Program.cs
+++ uygulama1/Program.cs
@@ -6,12 +6,15 @@ public class Program
     {
 
         // 1. Öðretmen oluþtur
+
         ITeacher teacher = new Teacher("Mustafa Kemal", "Atatürk");
 
         // 2. Sýnýf oluþtur ve öðretmeni baðla
+
         ClassRoom classRoom = new ClassRoom(teacher);
 
         // 3. Öðretmenin bilgilerini yazdýr
+
         Console.WriteLine(classRoom.GetTeacherInfo());
 
     }
